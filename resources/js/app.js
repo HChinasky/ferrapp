55d7a2d31eaf5c63b8fcc13535a78fcde5101c0f
Yaroslav <yarik_watson@hotmail.com>
2019-02-16 09:29:08 +0200

    laravel passport

--- resources/js/app.js
+++ resources/js/app.js
@@ -38,8 +38,9 @@ window.Toast = Toast;
 
 let routes = [
     { path: '/dashboard',  component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Developer.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
-    { path: '/profile', component: require('./components/Profile.vue').default }    
+    { path: '/profile', component: require('./components/Profile.vue').default } 
 ]
 
 const router = new VueRouter({
@@ -75,6 +76,21 @@ Vue.use(VueProgressBar, {
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
